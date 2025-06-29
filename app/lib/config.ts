075ed33e24f541f3f93a0b8706b6e2e254f4401b
Advay Chandramouli <advaycmouli@gmail.com>
2025-06-28 21:36:04 -0400

    Updated homepage

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Advay Chandramouli",
+  name: "Advay Chandramouli",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/advaycmouli",
+  github: "https://github.com/AdvayChandramouli",
+  instagram: "https://www.instagram.com/advaychandramouli",
+  linkedin: "https://www.linkedin.com/in/advaychandramouli",
+  email: "mailto:advaycmouli@gmail.com",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,56 +6,26 @@ export default function Page() {
     <section>
       <a href={socialLinks.twitter} target="_blank">
         <Image
-          src="/profile.png"
-          alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          src="/headshot.jpg"
+          alt="Advay Chandramouli headshot"
+          className=" bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 shadow-2xl border-3 border-white hover:grayscale-0"
           unoptimized
-          width={160}
-          height={160}
+          width={300}
+          height={300}
           priority
         />
       </a>
-      <h1 className="mb-8 text-2xl font-medium">Portfolio template!</h1>
+      <h2 className="text-2xl font-bold mb-4">About Me</h2>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS.
+          I'm a junior at The University of Texas at Dallas, double majoring in Computer Science & Cognitive Science. 
         </p>
         <p>
-          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
-          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
+          I'm passionate about building human-centered AI systems that blend intuitive design with impactful innovation.
+          Recently, I've worked on machine learning pipelines for preventative healthcare diagnostics and treatment recommendations.
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
-        </p>
-        <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
+           This summer, I joined Marshall University's NSF REU in Data Analytics under Dr. Husnu Narman to research computer vision approaches for railroad infrastructure monitoring. Later this Fall, I’ll be joining Verizon’s 5G RF Engineering team in Houston, TX.
         </p>
       </div>
     </section>
